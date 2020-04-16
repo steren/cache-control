c98cd352f15a9d8845c1d3a58cac609ec9a1f189
steren <steren@google.com>
2020-04-15 22:40:17 -0700

    Test app

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # cache-control
-Simple server returning cache-control headers.
+Simple server returning responses with cache-control headers set via query parameters.
+
+API: `/?maxAge=30&sMaxAge=300`
+
+Read more about cache-control headers at: 
+
+* https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Cache-Control
+* https://developers.google.com/web/fundamentals/performance/optimizing-content-efficiency/http-caching#cache-contro
---
+++ index.js
@@ -0,0 +1,25 @@
+import express from 'express';
+
+const app = express();
+ 
+app.get('/', (req, res) => {
+  let maxAge = req.query.maxAge || 30;
+  let sMaxAge = req.query.sMaxAge || 300;
+
+  let header = `public, max-age=${maxAge}, s-maxage=${sMaxAge}`;
+
+  res.set('Cache-Control', header);
+  res.send(`
+    API: <a href="/?maxAge=${maxAge}&sMaxAge=${sMaxAge}">/?maxAge=${maxAge}&sMaxAge=${sMaxAge}</a><br>
+    Warning: Chrome will ignore the max-age header if you use the Reload button, paste URL in new tab.<br>
+    This request:<br>
+    Cache-Control: ${header}<br>
+    Date recorded:<br>
+    ${new Date()}
+  ` );
+});
+
+const port = process.env.PORT || 8080;
+app.listen(port, () => {
+  console.log('I am listening on port', port);
+});
